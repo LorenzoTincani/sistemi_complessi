6b330e6e0a9b48e13d7a23921a9f34d1d7465dba
nettilaria <ilaria.netti@studio.unibo.it>
2022-03-13 22:56:41 +0100

    ho aggiunto nodi

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <random>
 int main()                                          //le modifiche vanno sus
 {
-    int const N = 10;                               //N è numero di nodi, N^2-N il numero di link possibili
+    int const N = 1000;                               //N è numero di nodi, N^2-N il numero di link possibili
     std::array<std::array<int, N>, N> adj_matrix{0}; //matrice di adiacenza, con int come pesi
     std::array<Building, N> nodes;
     double Total_potential=0.0;
