8cf50ab20738410e9d8b249087e532e96803b2a6
nettilaria <ilaria.netti@studio.unibo.it>
2022-03-17 23:58:23 +0100

    risoluzione problemi

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include <iostream>
 #include <array>
 #include <random>
+#include <cstdlib>
+#include <iomanip>
 int main() //le modifiche vanno sus
 {
-    int const N = 1000;                              //N è numero di nodi, N^2-N il numero di link possibili
+    std::cout << "riga 9 \n";
+    int const N = 30;                                //N è numero di nodi, N^2-N il numero di link possibili
     std::array<std::array<int, N>, N> adj_matrix{0}; //matrice di adiacenza, con int come pesi
     std::array<Building, N> nodes;
     double Total_potential = 0.0;
-    int nofSorting =0;
-
+    int nofSorting = 0;
+    int nofCentral = 0;
+    int nofHouse = 0;
     std::random_device rd; //   seed
     std::default_random_engine gen(rd());
     //std::discrete_distribution<int> link_dist({4, 3, 2, 1}); // parametri tra 0 e 3, per tre link più link non esistente
@@ -18,7 +22,7 @@ int main() //le modifiche vanno sus
     std::discrete_distribution<int> nodeType_dist({10, 3, 1});
     std::normal_distribution<double> needfluct_dist(0.0, 0.33); //distribuzione guassiana di fluttuazioni nella richiesta di energia delle case
     std::uniform_real_distribution<double> link_dist(0.0, 1.0); //distribuzione uniforme per distribuzione dei link, generato come proporzioni programma arcelli
-
+    std::cout << "riga 24 \n";
     for (int k = 0; k < N; ++k) //settaggio dei nodi
     {
         int numtype = nodeType_dist(gen); //numtype = type of node
@@ -27,13 +31,13 @@ int main() //le modifiche vanno sus
             double fluct = needfluct_dist(gen);
             nodes[k].SetNeed(7.2 + fluct); //consumo medio giornaliero di una famiglia media in kW/h
             //std::cout<<"house " <<nodes[k].GetNeed()<<std::endl;
+            nofHouse++;
         }
         else if (numtype == 1)
         {
             nodes[k].SetType(BuildingType::S);
             nofSorting++;
-            
-            
+
             //std::cout<<"sorting "<<nodes[k].GetNeed()<<std::endl;
         }
 
@@ -44,15 +48,20 @@ int main() //le modifiche vanno sus
             // std::cout<<"central "<<nodes[k].GetNeed()<<std::endl;
             nodes[k].SetEntryPotential(entrypotcentral); //da aggiustare
             Total_potential += entrypotcentral;
+            nofCentral++;
         }
     }
-
+    std::cout << "riga 59 \n";
+    int j = 0; //j è fuori per poter calcolare solo il triangolo superiore della matrice dato che è simmetrica
+    std::cout<<"nofcentral: "<<nofCentral<<std::endl;
+    std::cout<<"nofHouse: "<<nofHouse<<std::endl;
+    std::cout<<"nofSorting: "<<nofSorting<<std::endl;
     for (int i = 0; i < N; ++i)
     {
         BuildingType node_i = nodes[i].GetType();
 
         //DA GENERARE MATRICE DI ADIACENZA, CONTROLLANDO LA NATURA DEI VARI NODI.
-        for (int j = 0; j < N; ++j)
+        for (j; j < N; ++j)
         {
             double rnd = link_dist(gen); //generazione variabile uniforme della probabilità che avvenga link
             BuildingType node_j = nodes[j].GetType();
@@ -63,54 +72,76 @@ int main() //le modifiche vanno sus
             else
             {
                 if (node_i == BuildingType::H && node_j == BuildingType::H)
-                { //casa-casa
-                    if (rnd <= 0.05)//si suppone che, su 100 case, una casa sia collegata con altre 5. 
+                {                    //casa-casa
+                    if (rnd <= 0.05) //si suppone che, su 100 case, una casa sia collegata con altre 5.
                     {
-                        adj_matrix[i][j] = 1;
+                        adj_matrix[i][j] = 1; //link small
                     }
-                    else
-                    {
-                        adj_matrix[i][j] = 0;  //link small
-                    }
+                
                 }
                 else if (node_i == BuildingType::H && node_j == BuildingType::S)
-                { //casa-smistamento
-                    if (rnd <=(1/nofSorting)) //si suppone che la probabilità di avere questo link sia 1/# smistamento
+                {                                //casa-smistamento
+                    if (rnd <= (1 / nofSorting)) //si suppone che la probabilità di avere questo link sia 1/# smistamento
                     {
-                        adj_matrix[i][j] = 1;//Sempre 1 perchè l'energia che confluisce è sempre la medesima 
-                    }
-                    else
-                    {
-                        adj_matrix[i][j] = 0; // link medium
-                    }
+                        adj_matrix[i][j] = 1; //Sempre 1 perchè l'energia che confluisce è sempre la medesima
+                    } //link medium
+                
                 }
                 else if (node_i == BuildingType::S && node_j == BuildingType::S)
                 { //smistamento-smistamento
-                    if (rnd <=0.01)
+                    if (rnd <= 0.01)
                     {
                         adj_matrix[i][j] = 2;
                     }
-                    else
-                    {
-                        adj_matrix[i][j] = 0;
-                    }
+                
                 }
                 else
                 { //smistamento- centrale
-                //DA FARE. 
-                    if (rnd <=10)
-                    {
-                        adj_matrix[i][j] = 3;
-                    }
-                    else
-                    {
-                        adj_matrix[i][j] = 0;
-                    }
+                    bool p = false;
+                     float probCentral = 1 / nofCentral;
+                    double z=1;
+                   while(p==false){
+
+                        double rn= link_dist(gen);
+                        if (z<= probCentral)
+                       {
+                            adj_matrix[i][j] = 3;
+                            p = true;
+                        }
+                        z=z-rn;
+                    
+                 }
                 }
             }
         }
+        j = j - N;
+        j++;
+    }
+    std::cout << "riga 124 \n";
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            if (adj_matrix[i][k] == 0)
+            {
+                printf("\033[33m0");
+            }
+            else if (adj_matrix[i][k] == 1)
+            {
+                //std::cout << "       ";
+                printf("\033[31m1");
+            }
+            else if (adj_matrix[i][k] == 2)
+            {
+                //std::cout << "       ";
+                printf("\033[32m2");
+            }
+            else if (adj_matrix[i][k] == 3)
+            {
+                // std::cout << "       ";
+                printf("\033[36m3");
+            }
+        }
+        std::cout << std::endl;
     }
 }
-
-//COMPITO: genera la natura dei nodi, inserendoli all'interno di un contenitore (array/ vec)
-//in modo da indicizzarli e facilitare la caratterizzazione del link che li connette
